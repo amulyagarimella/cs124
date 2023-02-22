16823f0fcc3c7be0eb89e1d2e3f81c0fac6389af
amulyagarimella <amu.garimella@gmail.com>
2023-02-22 15:47:31 -0500

    concat

--- prog1/graph_weights.py
+++ prog1/graph_weights.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-weights_durations = pd.read_csv("weights_durations_6.csv")
+weights_durations_1 = pd.read_csv("weights_durations_6.csv")
+weights_durations_2 = pd.read_csv("weights_durations_7.csv")
+pd.concat([weights_durations_1, weights_durations_2])
+
 # avg_over_dim = weights_durations.groupby("size").mean()
 def size_vs (y): 
     sns.lineplot(data=weights_durations, x="size", y=y, hue="dim")
