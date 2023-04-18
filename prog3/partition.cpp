087adfd94051bfe5ce24b73832a326521df51232
amulyagarimella <amu.garimella@gmail.com>
2023-04-18 12:22:13 -0400

    random hc - need to fix random number gen

--- prog3/partition.cpp
+++ prog3/partition.cpp
@@ -11,6 +11,12 @@
 
 using namespace std;
 
+random_device rd;
+mt19937 gen(rd());
+uniform_real_distribution<> dis(0.0, 1.0);
+
+// TODO thread safe random
+
 // Karmarkar-Karp algorithm
 /*
 TODO: test
@@ -71,7 +77,7 @@ Correctness: ?
 // overwrite provided sequence of signs with new ones
 vector<int> * generateRandomSigns (vector<int> * signs) {
     int n = signs->size();
-    srand(time(NULL));
+    // srand(time(NULL));
     for (int i = 0; i < n; ++i) {
         int val = rand() % 2;
         if (val == 0) {
@@ -119,10 +125,13 @@ Correctness: ?
 // overwrite provided sequence of signs with random move
 vector<int> * randomMove (vector<int> * signs) {
     int n = signs->size();
-    srand(time(NULL));
-    int i = rand() % n;
+    // srand(time(NULL));
+    int i = 0, j=0;
+    while (i == j) {
+        i = rand() % n;
+        j = rand() % n;
+    }
     (*signs)[i] = -(*signs)[i];
-    int j = rand() % n;
     int coinflip = rand() % 2;
     if (coinflip == 0) {
         (*signs)[j] = -(*signs)[j];
@@ -152,7 +161,6 @@ int hillClimbing (vector<long> * sequence, int max_iter) {
         res = residue(sequence, &signs);
         res2 = residue(sequence, &signs2);
         if (res > res2) {
-            // hopefully switching pointers will work
             signs = signs2;
         }
     }
@@ -186,15 +194,12 @@ int simulatedAnnealing (vector<long> * sequence, int max_iter) {
             signs = signs2;
         }
         else {
-            random_device rd;
-            mt19937 gen(rd());
-            uniform_real_distribution<> dis(0.0, 1.0);
             float prob = exp(-(res2-res)/cooling(i));
             if (dis(gen) <= prob) {
                 signs = signs2;
             }
         }
-        // res3 = residue(sequence, &signs3);
+        res3 = residue(sequence, &signs3);
         if (res3 > residue(sequence, &signs)) {
             // hopefully switching pointers will work
             signs3 = signs;
@@ -205,48 +210,127 @@ int simulatedAnnealing (vector<long> * sequence, int max_iter) {
 
 
 vector<int> generatePrepartition (int num_partitions) {
-    vector<int> A_prime (num_partitions);
-    
-    srand(time(NULL));
+    vector<int> P (num_partitions);
     for (int i = 0; i < num_partitions; ++i) {
-        A_prime[i] = rand() % (num_partitions);
+        // p[i] between 0 and n-1
+        P[i] = rand() % num_partitions;
     }
-    return A_prime;  
+    return P;  
 }
 
-vector<long> convertPrepartition (vector<long> * sequence, vector<int> * partition) {
+/*
+Essentially A′ is derived by resetting ai to be the sum of all values j with pj = i,
+*/
+vector<long> * convertPrepartition (vector<long> * sequence, vector<int> * partition, vector<long> * newSequence) {
     int n = sequence->size();
 
-    vector<long> A_prime(n, 0);
+    fill(newSequence->begin(), newSequence->end(), 0);
 
     for (int j = 0; j < n; ++j) {
-        A_prime[(*partition)[j]] += (*sequence)[j];
+        (*newSequence)[(*partition)[j]] += (*sequence)[j];
     }
 
-    return A_prime; 
+    return newSequence; 
+}
+
+/*
+Thinking of all possible solutions as a state space, a natural way to define neighbors of a
+solution P is as the set of all solutions that differ from P in just one place. 
+The interpretation is that we
+change the prepartitioning by changing the partition of one element. 
+A random move on this state space
+can be defined as follows. Choose two random indices i and j
+ from [1, n] with pi  != j and set pi to j.
+*/
+vector<int> * prepartitionRandomMove (vector<int> * partition, vector<int> * partition2) {
+    int n = partition->size();
+    int i = 0, j = 0;
+    while ((*partition)[i] == j) {
+        i = rand() % n;
+        j = rand() % n;
+    }
+    partition2 = partition;
+    (*partition2)[i] = j;
+    return partition2;
 }
 
 
 // PRR
 int prepartitionRepeatedRandom (vector<long> * sequence, int max_iter) {
     int n = sequence->size();
+    // srand(time(NULL));
+    // cout << "size" << n << "\n";
     vector<int> partition = generatePrepartition(n);
-    vector<long> newSequence = convertPrepartition(sequence, &partition);
+    vector<long> newSequence (n);
+    convertPrepartition(sequence, &partition, &newSequence);
     
     int res = karmarkarKarp(&newSequence);
-        
+    vector<int> partition2;
+    vector<long> newSequence2 (n);
     for (int i = 0; i < max_iter; ++i) {
-        vector<int> partition2 = generatePrepartition(n);
-        vector<long> newSequence2 = convertPrepartition(sequence, &partition2);
-
+        partition2 = generatePrepartition(n);
+        /*cout << "p2\n";
+        for (int i = 0; i < 3; ++i) {
+            cout << partition2[i] << "\n";
+        } */
+        convertPrepartition(sequence, &partition2, &newSequence2);
         int res2 = karmarkarKarp(&newSequence2);
         if (res > res2) {
-            // hopefully switching pointers will work
             res = res2;
         }
     }
     return res;
 }
+
+
+// PHC
+int prepartitionHillClimbing (vector<long> * sequence, int max_iter) {
+    int n = sequence->size();
+    // srand(time(NULL));
+    // cout << "size" << n << "\n";
+    vector<int> partition = generatePrepartition(n);
+    vector<long> newSequence (n);
+    convertPrepartition(sequence, &partition, &newSequence);
+    int res = karmarkarKarp(&newSequence);
+    vector<int> partition2 = partition;
+    vector<long> newSequence2 (n);
+    int res2;
+    for (int i = 0; i < max_iter; ++i) {
+        // srand(time(NULL));
+        prepartitionRandomMove(&partition, &partition2);
+        /*cout << "p2\n";
+        for (int i = 0; i < 3; ++i) {
+            cout << partition[i] << "\n";
+        } */
+        convertPrepartition(sequence, &partition2, &newSequence2);
+        res2 = karmarkarKarp(&newSequence2);
+        if (res > res2) {
+            partition = partition2;
+        }
+    }
+    return karmarkarKarp(&newSequence);
+}
+
+// PSA TODO!!
+/*
+int prepartitionSimulatedAnnealing (vector<long> * sequence, int max_iter) {
+    int n = sequence->size();
+    // srand(time(NULL));
+    // cout << "size" << n << "\n";
+    vector<int> partition = generatePrepartition(n);
+    vector<long> newSequence = convertPrepartition(sequence, &partition);
+    int res = karmarkarKarp(&newSequence);
+    vector<long> newSequence2;
+    for (int i = 0; i < max_iter; ++i) {
+        prepartitionRandomMove(&partition);
+        newSequence2 = convertPrepartition(sequence, &partition);
+        int res2 = karmarkarKarp(&newSequence2);
+        if (res > res2) {
+            res = res2;
+        }
+    }
+    return res;
+}*/
 
 
 // Preprocessing: TODO
@@ -255,23 +339,34 @@ TODO: test
 Correctness: ?
 */
 
-// Main: TODO
+// TODO!!!!!!! check srand use
+// Main
 // ./partition flag algorithm inputfile
 int main (int argc, char * argv[]) {
+    srand(time(NULL));
     int flag = strtol(argv[1], NULL, 10);
     int algorithm = strtol(argv[2], NULL, 10);
 
     string inputfile = argv[3];
     ifstream input(inputfile);
     int seq_len = 100;
-    vector<long> seq (seq_len);
+    vector<long> seq;
     string line;
     for (int i = 0; i < seq_len; ++i) {
         getline(input, line);
         seq.push_back(stol(line, NULL, 10));
     }
+
+    // min=29980 | avg=2191094.1
     
     int max_iter = 25000;
+
+    /*vector<int> example_p = {0, 1, 1, 3, 4};
+    vector<long> example_kk = {10,8,7,6,5};
+    vector<long> new_kk = convertPrepartition(&example_kk, &example_p);
+    for (int i = 0; i < int(new_kk.size()); ++i) {
+        cout << new_kk[i] << "\n";
+    }*/
 
     if (algorithm == 0) {
         cout << karmarkarKarp(&seq);
@@ -287,6 +382,9 @@ int main (int argc, char * argv[]) {
     }
     else if (algorithm == 11) {
         cout << prepartitionRepeatedRandom(&seq, max_iter);
+    }
+    else if (algorithm == 12) {
+        cout << prepartitionHillClimbing(&seq, max_iter);
     }
 }
 
